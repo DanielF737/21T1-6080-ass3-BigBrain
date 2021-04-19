53b53f997d285b7f9347333495410f9babd66fb7
DanielF737 <daniel@ferra.ro>
2021-04-19 11:02:00 +1000

    finished up to part 2.2.1 and refactored ui, progress on 2.2.2

--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -11,12 +11,18 @@ import Avatar from '@material-ui/core/Avatar'
 import { QuizContext } from './util/quiz'
 const api = 'http://localhost:5005/'
 
+/**
+ * Deletes the users auth token from the local storage, effectively signing them out
+ */
 function signOut () {
   if (localStorage.getItem('token')) {
     localStorage.removeItem('token')
   }
 }
 
+/**
+ * Makes the API call to create a new quiz from the currently signed in user
+ */
 async function newQuiz () {
   const data = {
     name: 'New Empty Quiz'
@@ -34,6 +40,10 @@ async function newQuiz () {
   await fetch(`${api}admin/quiz/new`, options)
 }
 
+/**
+ * Makes the api call to the specified quiz
+ * @param {Number} id The ID of the quiz to delete
+ */
 async function deleteQuiz (id) {
   const options = {
     method: 'DELETE',
@@ -46,6 +56,10 @@ async function deleteQuiz (id) {
   await fetch(`${api}admin/quiz/${id}`, options)
 }
 
+/**
+ * Makes the API call to get the quizzes from the signed in user
+ * @returns A list of all quiz objects
+ */
 async function getQuizzes () {
   const options = {
     method: 'GET',
@@ -64,9 +78,26 @@ async function getQuizzes () {
     out.id = ret.quizzes[i].id
     quizzes.push(out)
   }
-  return (quizzes)
+  return quizzes
 }
 
+/**
+ * Totals the time of an array of questions
+ * @param {Array} questions An array of question objects to total the time of
+ * @returns {Number} The time in seconds that it will take to complete quiz
+ */
+function quizTime (questions) {
+  let total = 0
+  for (let i = 0; i < questions.length; i++) {
+    total += questions[i].time
+  }
+  return total
+}
+
+/**
+ * Functional component containing a list of all quizes belonging to the current user
+ * @returns Component containing list of cards representing each quiz
+ */
 function Quizzes () {
   const context = React.useContext(QuizContext)
   const [quizzes, setQuizzes] = context.quizzes
@@ -86,10 +117,13 @@ function Quizzes () {
           {/* TODO fix the width */}
           <Card key={i.id}>
             <CardContent>
-              <Typography variant="h5" component="h2">
+              <Typography variant="h5">
                 {/* TODO make this inline */}
                 <Avatar alt="Quiz Thumbnail" src={i.thumbnail} />
                 {i.name}
+              </Typography>
+              <Typography variant="body1">
+                Questions: {i.questions.length} | Time: {quizTime(i.questions)}s
               </Typography>
             </CardContent>
             <CardActions>
@@ -119,8 +153,11 @@ function Quizzes () {
   )
 }
 
-// TODO make new quiz button functional
-function Dashboard (props) {
+/**
+ * Displays dashboard of a signed in admin user or redirects to login if not signed in
+ * @returns The dashboard element for a signed in user
+ */
+function Dashboard () {
   const history = useHistory()
   const context = React.useContext(QuizContext)
   const [quizCount, setQuizCount] = context.quizCount
