5a805e725097a57e96e57d83a95f22185262c877
DanielF737 <daniel@ferra.ro>
2021-04-22 21:29:06 +1000

    added cypress tests to package.json

--- backend/src/custom.js
+++ backend/src/custom.js
@@ -14,16 +14,16 @@ export const quizQuestionPublicReturn = question => {
     sols: question.solutions.length
   }
   if ('url' in question) {obj.url = question.url}
-  return obj
-}
+  return obj;
+};
 
 /*
  For a given data structure of a question, get the IDs of
  the correct answers (minimum 1).
 */
 export const quizQuestionGetCorrectAnswers = question => {
-  return question.solutions
-}
+  return question.solutions;
+};
 
 /*
  For a given data structure of a question, get the IDs of
@@ -34,13 +34,13 @@ export const quizQuestionGetAnswers = question => {
   for (let i = 0; i<question.answers.length; i++) {
     a.push(question.answers[i].id)
   }
-  return question.answers
-}
+  return question.answers;
+};
 
 /*
  For a given data structure of a question, get the duration
  of the question once it starts. (Seconds)
 */
 export const quizQuestionGetDuration = question => {
-  return question.time
-}
+  return question.time;
+};
